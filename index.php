d1f9c9c017967361964dda7fe34d32854078b215
Abhishek Zambare <abhishekzambare@gmail.com>
2019-07-20 16:29:33 +0530

    new pro

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 <html lang="en">
 <?php
 session_start();
-      if(isset($_SESSION["active_admin"]))
-      {
-            require_once('fp/required/settings.php');
-            require_once('fp/required/database.php');
-            include_once("header.php");
-            include_once("content.php");
-            include_once("footer.php"); 
-      }else{ echo"unautharised accces";}
+if(isset($_SESSION["active_admin"]))
+{
+    require_once('fp/required/settings.php');
+    require_once('fp/required/database.php');
+    require_once("header.php");
+    ?><div class="main-panel"><?php
+    require_once("content.php");
+    require_once("footer.php"); 
+    ?></div><?php
+}else{ echo"unautharised accces";}
 ?>
 </html>
